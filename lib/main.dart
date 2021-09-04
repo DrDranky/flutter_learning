27483defd590027ec15323876cde00609d3d112a
DrDranky <paul.ionut.bob@gmail.com>
2021-09-04 09:52:20 +0100

    .

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,18 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(context) {
-    return MaterialApp(home: Text('Hello!'));
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
+    return MaterialApp(home: Scaffold(
+      appBar: AppBar(title: Text('My First App'),),
+      body: Column(children: [
+        Text('The question!'),
+        ElevatedButton(child: Text('Answer 1'), onPressed: null),
+        ElevatedButton(child: Text('Answer 2'), onPressed: null),
+        ElevatedButton(child: Text('Answer 3'), onPressed: null),
+      ],),
+    ));
   }
 }
