5882fca6f5b10c0ea259d2e89f53f949df62505d
DrDranky <paul.ionut.bob@gmail.com>
2021-09-04 13:54:32 +0100

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './question.dart';
+import './answer.dart';
 
 // void main() {
 //   runApp(MyApp());
@@ -40,20 +41,9 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: [
             Question(questions.elementAt(_questionIndex)),
-            ElevatedButton(child: Text('Answer 1'), onPressed: _answerQuestion),
-            ElevatedButton(
-                child: Text('Answer 2'),
-                onPressed: () => print('Answer 2 chosen!')),
-            ElevatedButton(
-              child: Text('Answer 3'),
-              onPressed: () {
-                // ...
-                setState(() {
-                  _questionIndex++;
-                });
-                print('Answer 3 chosen!');
-              },
-            ),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
           ],
         ),
       ),
