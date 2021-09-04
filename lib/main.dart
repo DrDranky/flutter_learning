ed85b01562e744d6349b7a26e0a7a557ea7886f5
DrDranky <paul.ionut.bob@gmail.com>
2021-09-04 09:24:22 +0100

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(context) {
     return MaterialApp(home: Text('Hello!'));
   }
